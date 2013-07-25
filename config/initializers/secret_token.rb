5c0a639361e74bf5c0cfe1b52076310695cf1d3f
Amy Hua <foramyhua@gmail.com>
2013-07-25 00:54:59 -0700

    Basic calendar functionality with Rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calendar::Application.config.secret_token = '4a0453cdde96597bb84096aa199cf86b2ae82d8062a188477d7f82369dae2e160c258b67d5a0f87325abb043b6eb3c58fdba57421d1053f3f231d216232a10d7'
